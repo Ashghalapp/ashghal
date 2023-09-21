222bbce48b6889a236ae6767da0ce679335c7eda
MohammedAlsayani <mohammedalsayaniii@gmail.com>
2023-09-21 05:01:50 +0300

    empty comment

---
+++ lib/features/auth/presentation/screens/forgetpassord/successresetpassword.dart
@@ -0,0 +1,78 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/simple/get_view.dart';
+
+import '../../../../../config/app_colors.dart';
+
+import '../../../../../core/localization/localization_strings.dart';
+import '../../../../../core/widget/app_buttons.dart';
+
+
+
+
+
+
+class SuccesResetPassword extends StatelessWidget {
+  const SuccesResetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        title: Text(
+        LocalizationString.success,
+          style:  TextStyle(
+            // fontFamily: 'Cairo',
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+            color:AppColors.grey,
+          ),
+        ),
+        leading: MyCircularIconButton(
+          onPressed: () {
+            // Get.back();
+          },
+          iconData: Icons.arrow_back_ios,
+          iconColor: AppColors.icongray,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+             Center(
+              child: Icon(
+                Icons.check_circle_outline,
+                size: 200,
+                color:Theme.of(context).primaryColor,
+              ),
+            ),
+            Text(
+              textAlign: TextAlign.center,
+              LocalizationString.successResetPassword,
+              style: const TextStyle(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 14,
+                  height: 2,
+                  color: Colors.grey),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            MyGesterDedector(
+              text: LocalizationString.signIn,
+              onTap: () {
+                // controller.goToLogIn();
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
